ee62c4c123e9e98e4590615f280d641d5429cb21
MK776 <yaa070706511@gmail.com>
2022-05-25 17:22:02 +0900

    test

---
+++ nextjs-blog/pages/test/index.js
@@ -0,0 +1,23 @@
+import Layout from '../../components/layout'
+import Image from 'next/image'
+
+export async function getStaticProps({ params }) {
+    const res = await fetch('https://coffee.alexflipnote.dev/random.json')
+    const json = await res.json()
+
+    return {
+        props: {
+            res: json
+        },
+        revalidate: 10
+    }
+}
+
+export default function Test({ res }) {
+    return (
+        <Layout>
+            <p>ISR</p>
+            <p>{res}</p>
+        </Layout>
+    )
+}
